efd06449e26ddb5e15f206a17eecf05234657763
jeldahsalsabilah08 <147676414+jeldahsalsabilah08@users.noreply.github.com>
2025-04-16 18:34:03 +0800

    Add files via upload

---
+++ PRE-PRATIKUM/pre-2 spl.sql
@@ -0,0 +1,20 @@
+-- Nama : Wiwin arningsih 
+-- NIM : 23241099
+-- Kelas : C 
+
+-- Melihat database yang ada
+SHOW DATABASES;
+
+-- Memilih database
+USE mysql;
+
+-- Melihat tabel dalam database mysql
+SHOW TABLES;
+
+-- Mendeskripsikan sebuah tabel yang dipilih ex, tabel 'user'
+DESCRIBE user;
+
+-- Melihat isi tabel user, khusus untuk kolom Host, user, dan password
+SELECT host, user, password FROM user;
+
+
